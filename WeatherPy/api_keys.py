dd8e96ffdf1ef5c61f72efff84dc33b43c8d9949
Avinash Dhawan <avinash.dhawan@gmail.com>
2020-03-16 00:33:42 -0400

    Latest update

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "4e788a2bf7867a3ea53eb541cd7e6ece"
+weather_api_key = "5ed36d1d86b9d908e1fb13773ec49842"
 
 # Google API Key
 g_key = "AIzaSyAHjUEfjtunyFr9D6IJkGMYwLTIIjV3XpU"
